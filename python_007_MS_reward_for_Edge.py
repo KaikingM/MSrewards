dcefa600751b5a63ce5e558834de1982f8f279c6
KaikingM <140623184+KaikingM@users.noreply.github.com>
2023-08-15 23:29:13 +0900

    Add files via upload

--- python_007_MS_reward_for_Edge.py
+++ python_007_MS_reward_for_Edge.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 import time
-import chromedriver_binary
-import pickle
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
--- python_008_MS_reward_for_Chrome.py
+++ python_008_MS_reward_for_Chrome.py
@@ -1,16 +1,17 @@
 import time
-import chromedriver_binary
-import pickle
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import random
-import playsound
+
 driver = webdriver.Chrome()#'/path/to/chromedriver'  # Optional argument, if not specified will search path.
 
 driver.get('https://www.bing.com/');
 
 #time.sleep(5) # Let the user actually m
+# nh
+# 
+# see something!
 
 search_box = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[2]/form/div[1]/div/textarea")
 num=random.randint(0,1000)
