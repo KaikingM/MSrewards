6acdaca7d87b2c91299f37d0d6003e763f353db9
KaikingM <140623184+KaikingM@users.noreply.github.com>
2023-08-15 18:32:28 +0900

    Add files via upload

---
+++ python_007_MS_reward_for_Edge.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+import time
+import chromedriver_binary
+import pickle
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import random
+import kaipy
+#import playsound
+driver = webdriver.Edge()
+#executable_path="C:\\Users\\kaiki\\AppData\\Local\\Packages\\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\\LocalCache\\local-packages\\Python310\\Scripts\\msedgedriver.exe"
+time.sleep(2)
+driver.get("https://www.bing.com")
+time.sleep(15)
+
+search_box = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[3]/div[2]/form/div[1]/div/textarea")
+num=random.randint(0,1000)
+search_box.send_keys(num)
+time.sleep(1)
+search_box.submit()
+time.sleep(1)
+#URL習得：driver.current_url
+k=0
+for i in range(33):#6
+    search_box = driver.find_element(By.XPATH, "/html/body/header/form/div/textarea")#/html/body/header/form/div/textarea
+    num=random.randint(0,1000)
+    search_box.send_keys(num)
+    time.sleep(1)
+    search_box.submit()
+    time.sleep(1)
+#driver.quit()
+
+time.sleep(10) # Let the user actually see something!
+#driver.quit()
+
+driver.get("https://www.bing.com")
+
+
+kaipy.kanon()
+time.sleep(30)
+
+#playsound.playsound(r"music\sw_on.mp3")
+#search_box=driver.find_element(By.XPATH,"/html/body/fluent-design-system-provider/edge-chromium-page//div[5]/cs-header-core/div[2]/common-search-box-edgenext//cs-common-search-box//form/div[1]/input")
+#num=random.randint(0,1000)
+#search_box.send_keys(num)
+#search_box.submit()
+
+for i in range(13):
+    search_box = driver.find_element(By.XPATH, "/html/body/header/form/div[1]/textarea")
+    num=random.randint(0,1000)
+    search_box.send_keys(num)
+    time.sleep(1)
+    search_box.submit()
+    time.sleep(1)
+#/html/body/header/form/div/input[1]
+#/html/body/header/form/div[1]/textarea
---
+++ python_008_MS_reward_for_Chrome.py
@@ -0,0 +1,35 @@
+import time
+import chromedriver_binary
+import pickle
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import random
+import playsound
+driver = webdriver.Chrome()#'/path/to/chromedriver'  # Optional argument, if not specified will search path.
+
+driver.get('https://www.bing.com/');
+
+#time.sleep(5) # Let the user actually m
+# nh
+# 
+# see something!
+
+search_box = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[3]/div[2]/form/div[1]/div/textarea")
+num=random.randint(0,1000)
+search_box.send_keys(num)
+time.sleep(1)
+search_box.submit()
+#!!ここでログイン！！
+time.sleep(30)
+#playsound.playsound(r"music\sw_on.mp3")
+#URL習得：driver.current_url
+for i in range(35):
+    search_box = driver.find_element(By.XPATH, "/html/body/header/form/div/input[1]")
+    num=random.randint(0,1000)
+    search_box.send_keys(num)
+    time.sleep(1)
+    search_box.submit()
+    time.sleep(1)
+#mailad
+#pass
